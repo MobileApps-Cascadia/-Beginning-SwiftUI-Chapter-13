5fde7b718d2530581fc75af0d56d3fc0e1054d67
Frederic789 <113332830+Frederic789@users.noreply.github.com>
2023-08-17 06:00:23 -0700

    Update CascadiaClasses.swift

--- Chapter13/CascadiaClasses.swift
+++ Chapter13/CascadiaClasses.swift
@@ -1,4 +1,4 @@
-//
+//Modified cascadiaClasses
 //  BasicLists.swift
 //  Chapter13
 //
@@ -8,34 +8,114 @@
 import SwiftUI
 
 struct CascadiaClassesView: View {
+    // Create sample data for demonstration
     
- 
+    let disciplines: [Discipline] = [
+           Discipline(name: "Information Technology", abbreviation: "IT", courses: [
+               Course(courseNumber: "IT-MOB 381", title: "iOS Application Development 1"),
+               Course(courseNumber: "IT-MOB 382", title: "iOS Application Development 2"),
+               Course(courseNumber: "IT-ADV 483", title: "Advanced Web Development")
+           ]),
+           Discipline(name: "English", abbreviation: "ENG", courses: [
+               Course(courseNumber: "ENG 101", title: "English Composition"),
+               Course(courseNumber: "ENG 201", title: "Creative Writing"),
+               Course(courseNumber: "ENG 302", title: "Shakespearean Literature")
+           ]),
+           Discipline(name: "Mathematics", abbreviation: "MATH", courses: [
+               Course(courseNumber: "MATH 101", title: "Intro to Mathematics"),
+               Course(courseNumber: "MATH 201", title: "Advanced Calculus"),
+               Course(courseNumber: "MATH 305", title: "Linear Algebra")
+           ])
+       ]
     var body: some View {
-        VStack {
-            Text("Cascadia Classes")
-                .font(.largeTitle)
+        NavigationView{
+            VStack {
+                Text("Cascadia Classes")
+                    .font(.largeTitle)
+                
+                List(disciplines) { discipline in
+                    NavigationLink(destination: CourseListView(discipline: discipline)) {
+                        Text(discipline.name)
+                    }
+                }
+            }
+            .padding()
+            .navigationTitle("Cascadia Classes")
+        }
+    }
+}
 
+
+struct CourseListView: View {
+    let discipline: Discipline
+    
+    var body: some View {
+        List {
+            Section(header: Text("Courses for \(discipline.name)")) {
+                //ForEach(0..<3) { index in
+                // Text("Course \(index + 1)")
+                ForEach(discipline.courses){ course in
+                    HStack{
+                        Text("\(course.courseNumber): \(course.title)")
+                        if course.isEnrolled{
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                    .swipeActions {
+                           if !course.isEnrolled {
+                               Button(action: {
+                                   // Enroll the user in the course
+                                   disciplines.forEach { disc in
+                                       if let courseIndex = disc.courses.firstIndex(where: { $0.id == course.id }) {
+                                           disciplines[disc.id].courses[courseIndex].isEnrolled = true
+                                       }
+                                   }
+                               }) {
+                                   Text("Enroll")
+                               }
+                               .tint(.green)
+   .padding()
+}
+}
+                    
+                    
+                    
+                }
+                
+                
+            }
             
         }
-        .padding()
+    
+        .navigationTitle("Courses")
+        
     }
 }
 
+struct Course : Identifiable{
+    let id = UUID()
+    let courseNumber: String
+    let title: String
+    var isEnrolled: Bool = false
+    
+    init(courseNumber: String, title: String) {
+            self.courseNumber = courseNumber
+            self.title = title
+        }
+}
 
-// Like "IT-MOB", or "ENG" (English) or "MATH"
 struct Discipline: Identifiable {
     let id = UUID()
-    // add properties here
+    let name: String
+    let abbreviation: String
+    var courses: [Course]
 }
 
-// Like "IT-MOB 381" or "ENG 101" or "MATH &107"
-struct Course: Identifiable {
-    let id = UUID()
-    // add properties here
-}
-
-struct CascadiaClassesView_Previews: PreviewProvider {
-    static var previews: some View {
-        CascadiaClassesView()
+    struct CascadiaClassesView_Previews: PreviewProvider {
+        static var previews: some View {
+            CascadiaClassesView()
+            
+            
+        }
     }
-}
+
